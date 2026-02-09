c3b4a77e0be92420a4186fbe3bae68634faa5118
RajeshPenupothu <rajeshpenupothu418@gmail.com>
2026-02-09 15:14:32 +0530

    .

--- backend/src/main/java/com/trackmymoney/backend/dto/DashboardSummaryDTO.java
+++ backend/src/main/java/com/trackmymoney/backend/dto/DashboardSummaryDTO.java
@@ -1,47 +1,27 @@
 package com.trackmymoney.backend.dto;
 
 public class DashboardSummaryDTO {
-    private Double income;
-    private Double expense;
-    private Double borrowed;
-    private Double lent;
+    private Double totalIncome;
+    private Double totalExpense;
+    private Double totalBorrowed;
+    private Double totalLent;
 
-    public DashboardSummaryDTO(Double income, Double expense, Double borrowed, Double lent) {
-        this.income = income;
-        this.expense = expense;
-        this.borrowed = borrowed;
-        this.lent = lent;
+    public DashboardSummaryDTO(Double totalIncome, Double totalExpense, Double totalBorrowed, Double totalLent) {
+        this.totalIncome = totalIncome;
+        this.totalExpense = totalExpense;
+        this.totalBorrowed = totalBorrowed;
+        this.totalLent = totalLent;
     }
 
-    public Double getIncome() { 
-        return income; 
-    }
+    public Double getTotalIncome() { return totalIncome; }
+    public void setTotalIncome(Double totalIncome) { this.totalIncome = totalIncome; }
 
-    public Double getExpense() { 
-        return expense; 
-    }
+    public Double getTotalExpense() { return totalExpense; }
+    public void setTotalExpense(Double totalExpense) { this.totalExpense = totalExpense; }
 
-    public Double getBorrowed() { 
-        return borrowed; 
-    }
+    public Double getTotalBorrowed() { return totalBorrowed; }
+    public void setTotalBorrowed(Double totalBorrowed) { this.totalBorrowed = totalBorrowed; }
 
-    public Double getLent() { 
-        return lent; 
-    }
-
-    public void setIncome(Double income) { 
-        this.income = income; 
-    }
-
-    public void setExpense(Double expense) { 
-        this.expense = expense; 
-    }
-
-    public void setBorrowed(Double borrowed) { 
-        this.borrowed = borrowed; 
-    }
-
-    public void setLent(Double lent) { 
-        this.lent = lent; 
-    }
+    public Double getTotalLent() { return totalLent; }
+    public void setTotalLent(Double totalLent) { this.totalLent = totalLent; }
 }
